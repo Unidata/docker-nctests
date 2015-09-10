c444dfc2a26a6f00a1be56a16c9e0bcab0ae1484
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-10 15:18:16 -0600

    Updated squash script.

--- squash_all_images.sh
+++ squash_all_images.sh
@@ -8,13 +8,47 @@
 
 set -e
 
+DOHELP()
+{
+    echo "Usage: $0 [suffix]"
+    echo ""
+    echo "Suffix for loading script that is generated."
+    echo ""
+}
+
+if [ $# -lt 1 ]; then
+    DOHELP
+    exit 1
+fi
+
+OUTSCRIPT="load-docker-images-${1}.sh"
+
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
 IMGS="unidata/nctests:base unidata/nctests:base32 unidata/nctests:serial unidata/nctests:serial32 unidata/nctests:mpich unidata/nctests:mpich32 unidata/nctests:openmpi unidata/nctests:openmpi32"
 
+echo "#!/bin/bash" > ${OUTSCRIPT}
+echo "#" $(date) >> ${OUTSCRIPT}
+echo "#" >> ${OUTSCRIPT}
 for X in $IMGS; do
     echo $X
-    set -x
-    docker save $X | sudo TMPDIR=/sandbox/tmp docker-squash -verbose -t $X | docker load
-    set +x
+    OUTNAME=$(echo $X | sed "s/:/-/g" | sed "s/\//_/g").tar
+    echo "Squashing ${X} to ${OUTNAME}"
+    docker save $X | sudo docker-squash -verbose -t $X -o ${OUTNAME}
+    echo "Loading ${OUTNAME}"
+    docker load -i ${OUTNAME}
+    echo ""
+
+    # Add to loading script.
+    echo "echo Loading ${OUTNAME} as ${X}" >> ${OUTSCRIPT}
+    echo "docker load -i ${OUTNAME}" >> ${OUTSCRIPT}
+    echo "echo" >> ${OUTSCRIPT}
+
 done
+
+echo "echo Finished" >> ${OUTSCRIPT}
+
+echo ""
+echo "Created utility loading script ${OUTSCRIPT}."
+echo ""
+echo "Finished."
