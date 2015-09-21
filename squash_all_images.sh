df7f55e78ea460d696766d3a20c1d8b27f164994
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-21 13:12:37 -0600

    Bumping to latest hdf5.

--- squash_all_images.sh
+++ squash_all_images.sh
@@ -25,9 +25,7 @@ OUTSCRIPT="load-docker-images-${1}.sh"
 
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
-IMGS="unidata/nctests:base \
-unidata/nctests:base32 \
-unidata/nctests:serial \
+IMGS="unidata/nctests:serial \
 unidata/nctests:serial32 \
 unidata/nctests:mpich \
 unidata/nctests:mpich32 \
@@ -43,7 +41,7 @@ for X in $IMGS; do
 
     echo "Squashing ${X} to ${OUTNAME}"
     docker save $X | sudo docker-squash -verbose -t $X -o ${OUTNAME}
-    sudo chown wfisher:wfisher ${OUTNAME}
+    sudo chown wfisher ${OUTNAME}
     echo "Loading ${OUTNAME}"
     docker load -i ${OUTNAME}
     echo ""
@@ -58,7 +56,7 @@ chmod 755 ${OUTSCRIPT}
 
 echo "echo Finished" >> ${OUTSCRIPT}
 
-sudo chown wfisher:wfisher ${OUTSCRIPT}
+sudo chown wfisher ${OUTSCRIPT}
 echo ""
 echo "Created utility loading script ${OUTSCRIPT}."
 echo ""
