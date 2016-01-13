92c4d828093cfd58257fd67c2e311d9dc6f1c281
Ward Fisher <wfisher@ucar.edu>
2016-01-13 11:22:03 -0700

    Added files for running ABI-related comparisons and reports.

---
+++ tests-abi/README.md
@@ -0,0 +1,11 @@
+# Generate ABI Difference Reports
+
+This docker container generates ABI difference reports for two versions of netcdf.  It uses the tools maintained by `lvc` at http://github.com/lvc.
+
+Two versions are required, and an `/output` directory must be mapped.
+
+## Options
+
+## Usage
+
+    $ docker run -e OLDVER="OLDBRANCH" -e NEWVER="NEWBRANCH" -v $(pwd):/output unidata/nc-abidiff
---
+++ tests-abi/run_abi_diff.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#####
+# Script to run abi comparison tools.
+#####
+
+set -e
+
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+if [ "x$HELP" != "x" ]; then
+    cat README.md
+    exit
+fi
+
+if [ ! -d "/output" ]; then
+    cat README.md
+    exit
+fi
+
+TOPDIR=$(pwd)
+OLDBUILD="build-$OLDVER"
+NEWBUILD="build-$NEWVER"
+
+$TDIR="netcdf-c"
+git clone http://github.com/Unidata/netcdf-c $TDIR
+cd $TDIR
+mkdir $OLDBUILD
+mkdir $NEWBUILD
+
+pushd $OLDBUILD
+git checkout $OLDVER
+cmake .. -DCMAKE_C_FLAGS="-g -Og" -DENABLE_TESTS=OFF
+make -j 4
+abi-dumper liblib/libnetcdf.so -o /output/ABI-$OLDVER.dump -lver $OLDVER
+git reset --hard
+popd
+
+pushd $NEWBUILD
+git checkout $NEWVER
+cmake .. -DCMAKE_C_FLAGS="-g -Og" -DENABLE_TESTS=OFF
+make -j 4
+abi-dumper liblib/libnetcdf.so -o /output/ABI-$NEWVER.dump -lver $NEWVER
+popd
+
+cd /output
+abi-compliance-checker -l libnetcdf -old ABI-$OLDVER.dump -new ABI-$NEWVER.dump
+
+echo "Finished."
