eaa47957486dd97e79c7d3fea54bfa5984a5eb03
Ward Fisher <wfisher@ucar.edu>
2016-02-18 12:38:56 -0700

    Added parallel images to push script.

--- tests-continuous-integration/push_all_ci_images.sh
+++ tests-continuous-integration/push_all_ci_images.sh
@@ -12,3 +12,5 @@ docker push unidata/ncci:fedora23-x64
 docker push unidata/ncci:fedora22-x64
 docker push unidata/ncci:fedora21-x64
 docker push unidata/ncci:centos7-x64
+docker push unidata/ncci:trusty-openmpi-x64
+docker push unidata/ncci:trusty-mpich-x64
