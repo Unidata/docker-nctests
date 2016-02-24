1546555e7f5f69c663c7ac9b7090f7ec9ee87053
Ward Fisher <wfisher@ucar.edu>
2016-02-24 12:52:54 -0700

    Base must be built manually. docker compose works for everything beyond that.

--- tests-regression/retag-compose-images.sh
+++ tests-regression/retag-compose-images.sh
@@ -14,14 +14,10 @@ RETAG() {
 
 }
 
-
-
-RETAG testsregression_base      unidata/nctests:base
 RETAG testsregression_serial    unidata/nctests:serial
 RETAG testsregression_openmpi   unidata/nctests:openmpi
 RETAG testsregression_mpich     unidata/nctests:mpich
 
-RETAG testsregression_base32    unidata/nctests:base32
 RETAG testsregression_serial32  unidata/nctests:serial32
 RETAG testsregression_openmpi32 unidata/nctests:openmpi32
 RETAG testsregression_mpich32   unidata/nctests:mpich32
