334b664cf88df3b6060e37568dd9c32b96b038bf
Ward Fisher <wfisher@ucar.edu>
2017-10-05 16:51:16 -0600

    Additional tests.

--- tests-regression/ncpdq_tst.sh
+++ tests-regression/ncpdq_tst.sh
@@ -2,9 +2,11 @@
 # ncpdq_tst.sh var_nm
 
 set -e
+set -x
 
 ncks -H -C -m --cdl -v ${1} ~/in.nc
 ncpdq -O -C -v ${1} ~/in.nc ~/foo.nc
 ncks -H -C -m --cdl -v ${1} ~/foo.nc
 ncpdq -O -U -C -v ${1} ~/foo.nc ~/foo_upk.nc
 ncks -H -C -m --cdl -v ${1} ~/foo_upk.nc
+ncatted -O -a _FillValue,global,c,1,222 ~/in.nc ~/foo3.nc
