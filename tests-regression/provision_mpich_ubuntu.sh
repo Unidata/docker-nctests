908e7547e0b3a9d8615585dbcf7e89f831eae4ba
Ward Fisher <wfisher@ucar.edu>
2018-03-12 16:11:12 -0600

    Corrected logic error.

--- tests-regression/provision_mpich_ubuntu.sh
+++ tests-regression/provision_mpich_ubuntu.sh
@@ -20,7 +20,8 @@ rm -rf /root/hdf-4.2.13
 # those tests as well.
 ###
 
-tar -jxf /root/hdf5-1.10.1.tar.bz2 && cd /root/hdf5-1.10.1 && CFLAGS"-Wno-format-security" CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
+CFLAGS="-Wno-format-security"
+tar -jxf /root/hdf5-1.10.1.tar.bz2 && cd /root/hdf5-1.10.1 && CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
 
 cd /root
 rm -rf /root/hdf5-1.10.1
@@ -30,7 +31,8 @@ rm -rf /root/hdf5-1.10.1
 # run pnetcdf tests.
 ###
 
-tar /root/parallel-netcdf-1.8.1 && cd /root/parallel-netcdf-1.8.1 && CPPFLAGS="-fPIC" CC=`which mpicc` ./configure --prefix=/usr --disable-fortran && make -j 4 -k && sudo make install
+CPPFLAGS="-fPIC"
+tar /root/parallel-netcdf-1.8.1 && cd /root/parallel-netcdf-1.8.1 && CC=$(which mpicc) ./configure --prefix=/usr --disable-fortran && make -j 4 -k && sudo make install
 
 cd /root
 rm -rf parallel-netcdf-1.8.1
