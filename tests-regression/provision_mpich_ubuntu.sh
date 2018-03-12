0dcb1f9ae33f5a36c996f97bc67655bbc27669d2
Ward Fisher <wfisher@ucar.edu>
2018-03-12 16:05:35 -0600

    Added files to provision smaller mpich.

---
+++ tests-regression/provision_mpich_ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+cd /root
+
+
+###
+# Manually install hdf4 so that we can run
+# those tests as well.
+###
+
+tar -jxf /root/hdf-4.2.13.tar.bz2 && cd /root/hdf-4.2.13 && CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-netcdf --disable-fortran --prefix=/usr && make -j 4 && sudo make install
+
+cd /root
+rm -rf /root/hdf-4.2.13
+
+
+###
+# Manually install hdf5 so that we can run
+# those tests as well.
+###
+
+tar -jxf /root/hdf5-1.10.1.tar.bz2 && cd /root/hdf5-1.10.1 && CFLAGS"-Wno-format-security" CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
+
+cd /root
+rm -rf /root/hdf5-1.10.1
+
+###
+# Manually install pnetcdf so that we can
+# run pnetcdf tests.
+###
+
+tar /root/parallel-netcdf-1.8.1 && cd /root/parallel-netcdf-1.8.1 && CPPFLAGS="-fPIC" CC=`which mpicc` ./configure --prefix=/usr --disable-fortran && make -j 4 -k && sudo make install
+
+cd /root
+rm -rf parallel-netcdf-1.8.1
