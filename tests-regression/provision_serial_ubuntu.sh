4a7c6f752f64fe97e17eb248de3947205ae23962
Ward Fisher <wfisher@ucar.edu>
2018-03-12 13:55:15 -0600

    More reorganization

--- tests-regression/provision_serial_ubuntu.sh
+++ tests-regression/provision_serial_ubuntu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 ###
 # Manually install hdf4 so that we can run
 # those tests as well.
 ###
 
-cd /root && tar -jxf hdf-4.2.13.tar.bz2 && cd hdf-4.2.13 && ./configure --disable-static --enable-shared --disable-netcdf --disable-fortran --prefix=/usr && make -j 4 && sudo make install
-rm -rf hdf-4.2.13
+tar -jxf /root/hdf-4.2.13.tar.bz2 && cd /root/hdf-4.2.13 && ./configure --disable-static --enable-shared --disable-netcdf --disable-fortran --prefix=/usr && make -j 4 && sudo make install
+rm -rf /root/hdf-4.2.13
 
 
 ###
@@ -14,5 +16,5 @@ rm -rf hdf-4.2.13
 # those tests as well.
 ###
 
-cd /root && tar -jxf ${HDF5_VER}.tar.bz2 && cd ${HDF5_VER} && ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib && make -j 4 && sudo make install
+tar -jxf /root/${HDF5_FILE} && cd /root/${HDF5_VER} && ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib && make -j 4 && sudo make install
 rm -rf ${HDF5_VER}
