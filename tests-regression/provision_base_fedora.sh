d6b042733e2bc6486c73cc63a04593e21d3e53e2
Ward Fisher <wfisher@ucar.edu>
2018-04-01 01:16:52 -0600

    Updated fedora base provision script.

--- tests-regression/provision_base_fedora.sh
+++ tests-regression/provision_base_fedora.sh
@@ -21,7 +21,7 @@ echo "${CUSER} ALL=NOPASSWD: ALL" >> /etc/sudoers
 # Install common packages.
 ###
 
-dnf -y install m4 git libjpeg-turbo-devel libcurl-devel wget nano libtool bison autoconf curl zlib-devel zip gcc-gfortran gcc-c++ byacc dos2unix bzip2 flex python numpy cython antlr python2-setuptools python-devel
+dnf -y install m4 git libjpeg-turbo-devel libcurl-devel wget nano libtool bison autoconf curl zlib-devel zip gcc-gfortran gcc-c++ byacc dos2unix bzip2 flex python numpy cython antlr python2-setuptools python-devel dos2unix
 
 
 ###
