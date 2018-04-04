fe0ff9573c308ff1938262b4aed5dd3bb7eb506e
Ward Fisher <wfisher@ucar.edu>
2018-04-04 09:19:24 -0600

    Added xenial parallel to build scripts.

--- tests-continuous-integration/build_all_images.sh
+++ tests-continuous-integration/build_all_images.sh
@@ -10,6 +10,18 @@ xterm -geometry 150x20 -bg black -fg white -T "[Ubuntu Xenial: 32-bit]" -e "dock
 sleep 1
 
 ###
+# Xenial - Parallel
+###
+
+xterm -geometry 150x20 -bg black -fg white -T "[Ubuntu Xenial: OpenMPI]" -e "docker build -t unidata/ncci:xenial-openmpi-x64 -f Dockerfile.xenial-openmpi-x64 . ; echo "" &&  echo '[Press Return to Close]' && read " &
+sleep 1
+
+xterm -geometry 150x20 -bg black -fg white -T "[Ubuntu Xenial: MPICH]" -e "docker build -t unidata/ncci:xenial-mpich-x64 -f Dockerfile.xenial-mpich-x64 . ; echo "" &&  echo '[Press Return to Close]' && read " &
+sleep 1
+
+
+
+###
 # Fedora
 ###
 xterm -geometry 150x20 -bg black -fg white -T "[Fedora 26: 64-bit]" -e "docker build -t unidata/ncci:fedora26-x64 -f Dockerfile.fedora26-x64 . ; echo "" &&  echo '[Press Return to Close]' && read " &
