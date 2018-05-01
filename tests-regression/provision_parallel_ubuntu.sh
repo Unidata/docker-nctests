de06acba8669eb61801b4bc05c3430a4a5185066
Ward Fisher <wfisher@ucar.edu>
2018-05-01 10:09:33 -0600

    Bumped up to HDF5 1.10.2

--- tests-regression/provision_parallel_ubuntu.sh
+++ tests-regression/provision_parallel_ubuntu.sh
@@ -21,10 +21,10 @@ rm -rf /root/hdf-4.2.13
 ###
 
 CFLAGS="-Wno-format-security"
-tar -jxf /root/hdf5-1.10.1.tar.bz2 && cd /root/hdf5-1.10.1 && CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
+tar -jxf /root/hdf5-1.10.2.tar.bz2 && cd /root/hdf5-1.10.2 && CC=$(which mpicc) ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
 
 cd /root
-rm -rf /root/hdf5-1.10.1
+rm -rf /root/hdf5-1.10.2
 
 ###
 # Manually install pnetcdf so that we can
