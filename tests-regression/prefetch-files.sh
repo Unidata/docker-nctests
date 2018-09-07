658cb7daba879494ca291ad47cacdd44f9a3d244
Ward Fisher <wfisher@ucar.edu>
2018-09-07 16:22:57 -0600

    Turn off NCO by default, use package-provided szip.

--- tests-regression/prefetch-files.sh
+++ tests-regression/prefetch-files.sh
@@ -5,7 +5,7 @@
 ##
 
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.2/src/hdf5-1.10.2.tar.bz2
-wget http://www.hdfgroup.org/ftp/lib-external/szip/2.1.1/src/szip-2.1.1.tar.gz
+#wget http://www.hdfgroup.org/ftp/lib-external/szip/2.1.1/src/szip-2.1.1.tar.gz
 wget http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-1.9.0.tar.gz
 wget https://hdfgroup.org/ftp/HDF/HDF_Current/src/hdf-4.2.13.tar.bz2
 wget https://cmake.org/files/v3.11/cmake-3.11.2.tar.gz
