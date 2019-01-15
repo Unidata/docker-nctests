15f948cfead366fbe42fc0a1e763fe9dd9a5432e
Ward Fisher <wfisher@ucar.edu>
2019-01-15 13:58:54 -0700

    Bumped 1.8.x version.

--- tests-regression/prefetch-files.sh
+++ tests-regression/prefetch-files.sh
@@ -4,7 +4,7 @@
 # Download the files so that each build can copy locally.
 ##
 
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.20/src/hdf5-1.8.20.tar.bz2
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.21/src/hdf5-1.8.21.tar.bz2
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.3/src/hdf5-1.10.3.tar.bz2
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.4/src/hdf5-1.10.4.tar.bz2
 wget http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-1.9.0.tar.gz
