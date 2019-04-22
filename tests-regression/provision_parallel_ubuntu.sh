6aa2383dd8ce93ae4c386727e572718e72431f15
Ward Fisher <wfisher@ucar.edu>
2019-04-22 10:47:10 -0600

    Bumped to hdf5 1.10.5

--- tests-regression/provision_parallel_ubuntu.sh
+++ tests-regression/provision_parallel_ubuntu.sh
@@ -21,10 +21,10 @@ rm -rf /root/hdf-4.2.14
 ###
 
 CFLAGS="-Wno-format-security"
-tar -jxf /root/hdf5-1.10.4.tar.bz2 && cd /root/hdf5-1.10.4 && CC=mpicc ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
+tar -jxf /root/hdf5-1.10.5.tar.bz2 && cd /root/hdf5-1.10.5 && CC=mpicc ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib --enable-parallel && make -j 4 && sudo make install
 
 cd /root
-rm -rf /root/hdf5-1.10.4
+rm -rf /root/hdf5-1.10.5
 
 ###
 # Manually install pnetcdf so that we can
--- tests-regression/provision_serial_ubuntu.sh
+++ tests-regression/provision_serial_ubuntu.sh
@@ -19,7 +19,7 @@ rm -rf /root/hdf-4.2.14
 # those tests as well.
 ###
 
-tar -jxf /root/hdf5-1.10.4.tar.bz2 && cd /root/hdf5-1.10.4 && ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib && make -j 4 && sudo make install
+tar -jxf /root/hdf5-1.10.5.tar.bz2 && cd /root/hdf5-1.10.5 && ./configure --disable-static --enable-shared --disable-fortran --enable-hl --prefix=/usr --with-szlib && make -j 4 && sudo make install
 
 cd /root
-rm -rf /root/hdf5-1.10.4
+rm -rf /root/hdf5-1.10.5
