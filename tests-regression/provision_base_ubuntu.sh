a09e259f200fde37e89360b5b12a795051930c60
Ward Fisher <wfisher@ucar.edu>
2022-01-05 13:57:01 -0700

    Install cmake from package manager

--- tests-regression/provision_base_ubuntu.sh
+++ tests-regression/provision_base_ubuntu.sh
@@ -8,7 +8,7 @@ set -e
 apt-get update
 apt-get -y upgrade
 apt-get -y install --no-install-recommends sudo
-
+apt-get -y install ca-certificates
 ##
 # Set up a non-root admin to run the tests as.
 ##
@@ -21,7 +21,7 @@ echo "${CUSER} ALL=NOPASSWD: ALL" >> /etc/sudoers
 ###
 # Install some basics.
 ###
-sudo apt-get -y install --no-install-recommends bzip2 g++ gfortran libtool automake autoconf m4 bison flex libcurl4-openssl-dev zlib1g-dev git wget curl libjpeg-dev cmake python-dev gdb dos2unix gsl-bin libgsl0-dev udunits-bin libudunits2-0 libudunits2-dev clang zip valgrind python-setuptools make build-essential less unzip patch libsz2 libaec-dev libssl-dev
+sudo apt-get -y install --no-install-recommends bzip2 g++ gfortran libtool automake autoconf m4 bison flex libcurl4-openssl-dev zlib1g-dev git wget curl libjpeg-dev cmake python-dev gdb dos2unix gsl-bin libgsl0-dev udunits-bin libudunits2-0 libudunits2-dev clang zip valgrind python-setuptools make build-essential less unzip patch libsz2 libaec-dev libssl-dev cmake
 
 ###
 # Uncompress tarballs.
@@ -30,7 +30,7 @@ sudo apt-get -y install --no-install-recommends bzip2 g++ gfortran libtool autom
 ##
 # Install cmake manually
 ##
-tar -zxf cmake-3.21.2.tar.gz && cd cmake-3.21.2 && ./configure --prefix=/usr && make -j 8 && sudo make install -j 8
+#tar -zxf cmake-3.21.2.tar.gz && cd cmake-3.21.2 && ./configure --prefix=/usr && make -j 8 && sudo make install -j 8
 
 ##
 # Some cleanup
