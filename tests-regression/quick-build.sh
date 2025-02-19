72c57eb7e9de225b4c096e8363daeaaa4826cdab
Ward Fisher <wfisher@ucar.edu>
2025-02-19 10:05:52 -0700

    Added quick-build script. This builds the images for linux/arm64,linux/amd64, no-cache, and pushes to repository.

---
+++ tests-regression/quick-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Simple, brute force build
+
+set -e
+
+echo -e "Building and Pushing images (linux/arm64,linux/amd64), no-cache"
+echo -e "==============================================================="
+echo -e ""
+
+echo -e "\to Base"
+sleep 2
+time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:base -f Dockerfile.base . --no-cache --push
+
+echo -e "\to Serial"
+sleep 2 
+time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:serial -f Dockerfile.serial . --no-cache --push
+
+echo -e "\to MPICH"
+sleep 2 
+time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:mpich -f Dockerfile.mpich . --no-cache --push
+
+
+
