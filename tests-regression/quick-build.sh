f720c8c3c04e7847ce296494da16f0afd454f53a
Ward Fisher <wfisher@ucar.edu>
2025-02-19 10:10:29 -0700

    Adjust spacing in output.

--- tests-regression/quick-build.sh
+++ tests-regression/quick-build.sh
@@ -5,16 +5,18 @@ set -e
 
 echo -e "Building and Pushing images (linux/arm64,linux/amd64), no-cache"
 echo -e "==============================================================="
-echo -e ""
 
+echo -e ""
 echo -e "\to Base"
 sleep 2
 time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:base -f Dockerfile.base . --no-cache --push
 
+echo -e ""
 echo -e "\to Serial"
 sleep 2 
 time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:serial -f Dockerfile.serial . --no-cache --push
 
+echo -e ""
 echo -e "\to MPICH"
 sleep 2 
 time docker build --platform linux/arm64,linux/amd64 -t docker.unidata.ucar.edu/nctests:mpich -f Dockerfile.mpich . --no-cache --push
