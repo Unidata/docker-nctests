db6ac6439299e5c7b44b6c9f0002d9464c6a2508
Ward Fisher <wfisher@ucar.edu>
2025-03-04 11:32:49 -0700

    Further refine test script.

--- tests-regression/test-docker-container.sh
+++ tests-regression/test-docker-container.sh
@@ -61,13 +61,15 @@ echo -e "\to NetCDF-C"
 git clone git@github.com:Unidata/netcdf-c --single-branch --branch ${NCVER} ${NCDIR}  >> ${LOGFILE} 2>&1 ; CHECKERR
 echo -e "\to NetCDF-Fortran"
 git clone git@github.com:Unidata/netcdf-fortran --single-branch --branch ${NFVER} ${NFDIR}  >> ${LOGFILE} 2>&1 ; CHECKERR
+echo -e "\to NetCDF-Java"
+git clone git@github.com:Unidata/netcdf-java --single-branch --branch ${NJVER} ${NJDIR}  >> ${LOGFILE} 2>&1 ; CHECKERR
 
 ###
 # Begin running tests
 ###
 
 
-DCMD="docker run --rm -it -v ${NCDIR}:/netcdf-c -v ${ENVDIR}:/environments -v ${NFDIR}:/netcdf-fortran docker.unidata.ucar.edu/nctests"
+DCMD="docker run --rm -it -v ${NCDIR}:/netcdf-c -v ${NFDIR}:/netcdf-fortran -v ${ENVDIR}:/environments -v ${NFDIR}:/netcdf-fortran docker.unidata.ucar.edu/nctests"
 echo ""
 echo -e "Running Baseline Docker Test:"
 echo -e "============================="
@@ -81,7 +83,7 @@ echo -e "======================================"
 ##
 # Run C Test
 ## 
-DCMD="docker run --rm -it -v ${NCDIR}:/github/workspace -v ${ENVDIR}:/environments -e GITHUB_ACTIONS="TRUE" -e REPO_TYPE="c" docker.unidata.ucar.edu/nctests"
+DCMD="docker run --rm -it -v ${NCDIR}:/github/workspace -f ${NFDIR}:/netcdf-fortran -v ${ENVDIR}:/environments -e GITHUB_ACTIONS="TRUE" -e REPO_TYPE="c" docker.unidata.ucar.edu/nctests"
 echo -e "\to NetCDF-C"
 echo -e "\t\to GITHUB_ACTIONS=TRUE"
 echo -e "\t\to REPO_TYPE=c"
@@ -92,7 +94,7 @@ ${DCMD} >> ${LOGFILE} 2>&1 ; CHECKERR
 ##
 # Run Fortran Test
 ##
-DCMD="docker run --rm -it -v ${NFDIR}:/github/workspace -v ${ENVDIR}:/environments -e GITHUB_ACTIONS="TRUE" -e RUNC=OFF -e REPO_TYPE="fortran" docker.unidata.ucar.edu/nctests"
+DCMD="docker run --rm -it -v ${NCDIR}:/netcdf-c -v ${NFDIR}:/github/workspace -v ${ENVDIR}:/environments  -e GITHUB_ACTIONS="TRUE" -e RUNC=OFF -e REPO_TYPE="fortran" docker.unidata.ucar.edu/nctests"
 echo -e "\to NetCDF-Fortran"
 echo -e "\t\to GITHUB_ACTIONS=TRUE"
 echo -e "\t\to REPO_TYPE=fortran"
@@ -103,7 +105,7 @@ ${DCMD} >> ${LOGFILE} 2>&1 ; CHECKERR
 ##
 # Run Java Test
 ##
-DCMD="docker run --rm -it -v ${NFDIR}:/github/workspace -v ${ENVDIR}:/environments -e GITHUB_ACTIONS="TRUE" -e RUNC=OFF -e RUNF=OFF -e RUNJAVA=TRUE -e REPO_TYPE="java" docker.unidata.ucar.edu/nctests"
+DCMD="docker run --rm -it -v ${NCDIR}:/netcdf-c -v ${NFDIR}:/netcdf-fortran -v ${NJDIR}:/github/workspace -v ${ENVDIR}:/environments -e GITHUB_ACTIONS="TRUE" -e RUNC=OFF -e RUNF=OFF -e RUNJAVA=TRUE -e REPO_TYPE="java" docker.unidata.ucar.edu/nctests"
 echo -e "\to NetCDF-Java"
 echo -e "\t\to GITHUB_ACTIONS=TRUE"
 echo -e "\t\to REPO_TYPE=java"
