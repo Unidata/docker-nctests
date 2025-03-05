29f122002fdf8624a464949d07427363057701e7
Ward Fisher <wfisher@ucar.edu>
2025-03-05 10:09:42 -0700

    Updated gitignore, adding libmpich dev package to baseline provision.

--- tests-regression/provision_base_ubuntu.sh
+++ tests-regression/provision_base_ubuntu.sh
@@ -22,7 +22,7 @@ echo "${CUSER} ALL=NOPASSWD: ALL" >> /etc/sudoers
 ###
 # Install some basics.
 ###
-sudo apt-get -y install --no-install-recommends bzip2 g++ gfortran libtool automake autoconf m4 bison flex libcurl4-openssl-dev zlib1g-dev git wget curl libjpeg-dev cmake gdb dos2unix gsl-bin libgsl0-dev udunits-bin libudunits2-0 libudunits2-dev clang zip valgrind python-setuptools-doc make build-essential less unzip patch libsz2 libaec-dev libssl-dev cmake libxml2 libxml2-dev mpich nano
+sudo apt-get -y install --no-install-recommends bzip2 g++ gfortran libtool automake autoconf m4 bison flex libcurl4-openssl-dev zlib1g-dev git wget curl libjpeg-dev cmake gdb dos2unix gsl-bin libgsl0-dev udunits-bin libudunits2-0 libudunits2-dev clang zip valgrind python-setuptools-doc make build-essential less unzip patch libsz2 libaec-dev libssl-dev cmake libxml2 libxml2-dev mpich nano libmpich-dev
 
 ###
 # Uncompress tarballs.
