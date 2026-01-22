4f798bfc4089be0f0caf047d2a5155c236d1fdde
Ward Fisher <wfisher@ucar.edu>
2026-01-22 13:42:55 -0700

    Skeleton for roughed-in file.

---
+++ tests-regression/install_intelone.sh
@@ -0,0 +1,6 @@
+# Install IntelOne compilers. 
+# See the following for installation, usage information.
+#
+# https://www.intel.com/content/www/us/en/docs/oneapi/installation-guide-linux/2023-0/apt.html
+# https://www.intel.com/content/www/us/en/docs/dpcpp-cpp-compiler/get-started-guide/2024-0/get-started-on-linux.html
+# https://www.intel.com/content/www/us/en/docs/oneapi/programming-guide/2025-1/use-the-setvars-and-oneapi-vars-scripts-with-linux.html
