2ce433ec4741f5062897256aa265e7293d0df750
Ward Fisher <wfisher@ucar.edu>
2026-03-04 16:41:44 -0700

    H4VER is now empty by default, and if not specified, hdf4 tests aren't run.

--- tests-regression/run_netcdf_tests.sh
+++ tests-regression/run_netcdf_tests.sh
@@ -362,26 +362,33 @@ fi
 ###
 # Install HDF4
 ###
-echo "Installing HDF4 Version: ${H4VER}"
-echo ""
-sleep 2
 
-H4MAJ=$(echo $H4VER | cut -d '.' -f 1)
-H4MIN=$(echo $H4VER | cut -d '.' -f 2)
-H4REV=$(echo $H4VER | cut -d '.' -f 3)
-H4DIR="hdf4-hdf${H4VER}"
-H4FILE="hdf${H4VER}.tar.gz"
-H4URL="https://github.com/HDFGroup/hdf4/archive/refs/tags/${H4FILE}"
-wget "${H4URL}"
-tar -zxf "${H4FILE}"
-cd "${H4DIR}"
-autoreconf -if
-CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -fPIC" CC="${NCCOMP}" ./configure --prefix="${TARGDIR}" ${BUILDARGAC} --disable-netcdf --disable-fortran
-sleep 2
-${SUDOCMD} make install -j "${TESTPROC}"
-make clean -j "${TESTPROC}"
+if [ "${H4VER}" != "" ]; then
 
-cd ..
+    H4ACOPT="--enable-hdf4"
+    H4CMAKEOPT="-DNETCDF_ENABLE_HDF4=TRUE"
+
+    echo "Installing HDF4 Version: ${H4VER}"
+    echo ""
+    sleep 2
+
+    H4MAJ=$(echo $H4VER | cut -d '.' -f 1)
+    H4MIN=$(echo $H4VER | cut -d '.' -f 2)
+    H4REV=$(echo $H4VER | cut -d '.' -f 3)
+    H4DIR="hdf4-hdf${H4VER}"
+    H4FILE="hdf${H4VER}.tar.gz"
+    H4URL="https://github.com/HDFGroup/hdf4/archive/refs/tags/${H4FILE}"
+    wget "${H4URL}"
+    tar -zxf "${H4FILE}"
+    cd "${H4DIR}"
+    autoreconf -if
+    CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -fPIC" CC="${NCCOMP}" ./configure --prefix="${TARGDIR}" ${BUILDARGAC} --disable-netcdf --disable-fortran
+    sleep 2
+    ${SUDOCMD} make install -j "${TESTPROC}"
+    make clean -j "${TESTPROC}"
+
+    cd ..
+fi
 
 ###
 # End Install HDF4
@@ -550,7 +557,7 @@ while [[ $CCOUNT -le $CREPS ]]; do
         sleep 2
         mkdir -p build-netcdf-c
         cd build-netcdf-c
-        cmake ${WORKING_DIRECTORY}/netcdf-c -DCMAKE_INSTALL_PREFIX=${NC_TARGDIR} ${CMAKE_CDOC_OPTS} -DNETCDF_ENABLE_MMAP=ON -DNETCDF_ENABLE_HDF4=TRUE -DBUILDNAME_PREFIX="docker$BITNESS-$USE_CC" -DBUILDNAME_SUFFIX="$CBRANCH" -DCMAKE_C_COMPILER=$USE_CC -DCMAKE_CXX_COMPILER=${USE_CXX} ${CMAKE_PAR_OPTS} ${CMAKE_COPTS} ${S3OPTS_CMAKE} -DCMAKE_C_FLAGS="${CMEM}" -DENABLE_TESTS="${RUNC}"; CHECKERR
+        cmake ${WORKING_DIRECTORY}/netcdf-c -DCMAKE_INSTALL_PREFIX=${NC_TARGDIR} ${CMAKE_CDOC_OPTS} -DNETCDF_ENABLE_MMAP=ON "${H4CMAKEOPT}" -DBUILDNAME_PREFIX="docker$BITNESS-$USE_CC" -DBUILDNAME_SUFFIX="$CBRANCH" -DCMAKE_C_COMPILER=$USE_CC -DCMAKE_CXX_COMPILER=${USE_CXX} ${CMAKE_PAR_OPTS} ${CMAKE_COPTS} ${S3OPTS_CMAKE} -DCMAKE_C_FLAGS="${CMEM}" -DENABLE_TESTS="${RUNC}"; CHECKERR
         make clean
 
         if [ "x$RUNC" == "xTRUE" ]; then
@@ -594,7 +601,7 @@ while [[ $CCOUNT -le $CREPS ]]; do
         if [ ! -f "configure" ]; then
             autoreconf -if
         fi
-        CXX=$USE_CXX CC=$USE_CC ./configure --prefix=${NC_TARGDIR} ${AC_PAR_OPTS} ${AC_CDOC_OPTS} --enable-hdf4 --enable-extra-tests --enable-mmap ${AC_COPTS} ${S3OPTS_AC}
+        CXX=$USE_CXX CC=$USE_CC ./configure --prefix=${NC_TARGDIR} ${AC_PAR_OPTS} ${AC_CDOC_OPTS} ${H4ACOPT} --enable-extra-tests --enable-mmap ${AC_COPTS} ${S3OPTS_AC}
         make clean
         make -j $TESTPROC ; CHECKERR
         if [ "x$RUNC" == "xTRUE" ]; then
@@ -602,7 +609,7 @@ while [[ $CCOUNT -le $CREPS ]]; do
             make check -j $TESTPROC ; CHECKERR_AC
 
             if [ "x$DISTCHECK" == "xTRUE" ]; then
-                DISTCHECK_CONFIGURE_FLAGS="--enable-hdf4 --enable-extra-tests --enable-mmap ${S3OPTS_AC} ${AC_COPTS}" make distcheck ; CHECKERR
+                DISTCHECK_CONFIGURE_FLAGS="${H4ACOPT} --enable-extra-tests --enable-mmap ${S3OPTS_AC} ${AC_COPTS}" make distcheck ; CHECKERR
             fi
 
         fi
